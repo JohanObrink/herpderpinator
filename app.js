9acdf0f64c47782362d47b022b06d47e7eb62e92
Johan Öbrink <johan.obrink@24hr.se>
2012-10-03 10:58:17 +0200

    Added Procfile and npm script for start

--- app.js
+++ app.js
@@ -27,6 +27,18 @@ mongoose.model('User', UserSchema);
 
 var User = mongoose.model('User');
 
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
 var app = express();
 
 app.configure(function(){
